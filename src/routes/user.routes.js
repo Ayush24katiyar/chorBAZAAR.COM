3767f95949df55baf304ebfe81459f33f84ba4fc
Ayush Katiyar <ayush24katiyar@gmail.com>
2025-09-14 15:11:15 +0530

    : : created user routes successfully !

---
+++ src/routes/user.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+
+// USER
+
+import { 
+    registerUSER ,
+    loginUSER ,
+    logoutUSER
+
+ } from "../controllers/USER.controllers";
+ 
+
+ 
+
+
+ const router = Router()
+
+ // USER ROUTES 
+
+ router.route("/register").post(registerUSER)
+ router.route("/login").post(verifyJWT , loginUSER)
+ router.route("/logout").post(verifyJWT ,  logoutUSER)
+
+
+ export default router;
