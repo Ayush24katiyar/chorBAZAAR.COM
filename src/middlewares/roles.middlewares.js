464d420d1578c2e127cc6ec43021a5379fc2ce84
Ayush Katiyar <ayush24katiyar@gmail.com>
2025-09-14 19:43:35 +0530

    : : created role middleware

---
+++ src/middlewares/roles.middlewares.js
@@ -0,0 +1,10 @@
+import { asyncHandler } from "../utils/asynchandler.js"
+import { apierror } from "../utils/APIerror.js"
+const requiredROLE = (role) => asyncHandler(async (req ,res , next) => {
+if (!req.user) {
+    throw new apierror(400 , "user login is required !")
+}
+if(req.user.role !== role) {
+    throw new apierror(400 , "access denied u need to have seller role !")
+}
+})
