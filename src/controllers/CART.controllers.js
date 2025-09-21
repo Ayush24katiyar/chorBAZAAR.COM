b60edfbcd67236505074e5b7b22831cb250e2346
Ayush Katiyar <ayush24katiyar@gmail.com>
2025-09-21 14:34:09 +0530

    : : cart controller written successfully !

---
+++ src/controllers/CART.controllers.js
@@ -0,0 +1,70 @@
+import CART from "../models/cart.model.js";
+import { asyncHandler } from "../utils/asynchandler.js";
+import { apierror } from "../utils/APIerror.js";
+import { apiresponse } from "../utils/APIresponse.js";
+
+const AddProductToCart = asyncHandler(async (req , res) => {
+    const userID = req.user._id
+    const {productID , quantity} = req.body
+    //validation
+    if(!productID && !quantity || quantity < 1) {
+        throw new apierror(400 , "please enter the right values")
+    }
+
+    // find cart of user
+    const cart = await CART.findOne({userID})
+    if(!cart){
+        cart =  new CART({userID , items : []})
+    }
+
+    // check  if porduct is already exist in the cart or not 
+    const productINDEX = cart.items.findIndex(item => item.productID.toString() === productID)
+
+    if(productINDEX > -1){
+        cart.items[productINDEX].quantity += quantity
+
+    } else {
+        cart.items.push({productID , quantity})
+
+    }
+
+    await cart.save()
+    return res.status(200).json(new apiresponse(200 , cart , "everything is a success !"))
+})
+
+const DeleteProductFromCart = asyncHandler(async (req , res ) => {
+    // account login
+    const userID = req.user._id
+    const {productID} = req.body
+
+
+    if (!productID) {
+        throw new apierror(400 , "product id is required to remove item")
+    }
+
+     // user ki cart ko find kro
+
+     const cart = await CART.findOne({userID})
+     if(!cart) {
+        throw new apierror(400 , "user doesnt own the cart !")
+     }
+
+
+     // remove the item
+
+     cart.items = cart.items.filter(
+            (item) => item.productID.toString() !== productID
+     )
+
+     await cart.save()
+
+
+     return res.status(200).json(new apiresponse(200 , cart , "item remove successfully !"))
+})
+
+
+
+export {
+    DeleteProductFromCart , 
+    AddProductToCart
+}
--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,25 +1,30 @@
-import mongoose , {Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const cartSCHEMA = new Schema ({
-    userID : {
-        type : mongoose.Schema.ObjectId , 
-        ref : "USER" , 
-        required : true  ,
-    } , 
-    productID : [{
-        type : mongoose.Schema.ObjectId , 
-        ref : "PRODUCT" ,
-    } ], 
+const cartSCHEMA = new Schema(
+  {
+    userID: {
+      type: mongoose.Schema.ObjectId,
+      ref: "USER",
+      required: true,
+    },
+    items: [
+      {
+        productID: {
+          type: mongoose.Schema.ObjectId,
+          ref: "PRODUCT",
+        },
 
-    quantity : {
-        type : Number ,
-    }
-    
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-} ,
-{
-    timestamps : true
-}
-)
-
-export default CART =  mongoose.model("CART" , cartSCHEMA)
+export default CART = mongoose.model("CART", cartSCHEMA);
